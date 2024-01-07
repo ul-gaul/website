8c81dc0f47a87803afccd8b814f705988506345c
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2024-01-07 18:00:45 -0500

    Pages principales
    Ajout des component pour les pages principales

---
+++ src/app/espace/espace.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-espace',
+  standalone: true,
+  imports: [],
+  templateUrl: './espace.component.html',
+  styleUrl: './espace.component.scss'
+})
+export class EspaceComponent {
+
+}
---
+++ src/app/fusee/fusee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fusee',
+  standalone: true,
+  imports: [],
+  templateUrl: './fusee.component.html',
+  styleUrl: './fusee.component.scss'
+})
+export class FuseeComponent {
+
+}
