7805185d6fa69f9298b77b8856bcea726b9202c2
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2024-01-07 18:18:15 -0500

    Ajout des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { FuseeComponent } from './fusee/fusee.component';
+import { EspaceComponent } from './espace/espace.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'fusee', component: FuseeComponent },
+  { path: 'espace', component: EspaceComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
