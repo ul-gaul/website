15644b72803ef3e2514c9ddecf5cbd7bb0b6d88b
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2024-01-08 21:51:56 -0500

    Ajout page-fusee
    Copie de l'ancien site,
    
    Mise à jour du texte, des images et de la structure.

--- src/app/fusee/fusee.component.ts
+++ src/app/fusee/fusee.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-fusee',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './fusee.component.html',
   styleUrl: './fusee.component.scss'
 })
