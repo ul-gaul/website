0e8983d5fee15cc12a6140d50809c70921319d66
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2024-01-08 23:21:55 -0500

    Ajout de page-not-found
    Création de la page not found

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
