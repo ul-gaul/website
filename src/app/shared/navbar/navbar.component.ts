7758db8aa7c9c790bad8d3331c67df44edc248ce
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2024-01-08 19:15:25 -0500

    Navbar opaque
    Navbar opaque quand on scroll

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,22 +1,36 @@
-// import { Component } from '@angular/core';
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   // Variable qui contrôle l'ouverture de la sidebar
-  // Bind avec la classe
-  sidebarVisible = false;
+  // Bind avec la classe 'nav-open'
+  // Voir sidebarToggle()
+  public sidebarVisible = false;
 
   // EventEmitter pour envoier la variable sidebarVisible aux parents
   // lorsque la fonction sidebarToggle() est appelée
+  // Voir sidebarToggle()
   @Output() toggleSidebarEvent = new EventEmitter<boolean>();
+
+  // Variable qui contrôle la transparence de la navbar
+  // Bind avec la classe 'navbar-transparent'
+  // Voir window.onscroll
+  public navbarTransparent = true;
+
+  constructor() {
+    // Fonction appelée à chaque fois que l'utilisateur scroll
+    window.onscroll = () => {
+      this.navbarTransparent = window.scrollY < 150
+    }
+  }
 
   sidebarToggle() {
     // Inverse la variable (true -> false) (false -> true)
