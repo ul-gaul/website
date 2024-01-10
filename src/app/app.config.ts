aabc0f6494318053fa9e941811757d9af11e1ce5
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2024-01-09 23:34:12 -0500

    Update app.config.ts
    Scroll au top automatique quand on change de page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { InMemoryScrollingOptions, withInMemoryScrolling, provideRouter } from '@angular/router';
+import { IMAGE_CONFIG } from '@angular/common';
 
 import { routes } from './app.routes';
 
+const scrollingOptions: InMemoryScrollingOptions = {
+  // Scroll top on route change
+  scrollPositionRestoration: 'top',
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withInMemoryScrolling(scrollingOptions)),
+    {
+      // Disable Image Size Warning
+      provide: IMAGE_CONFIG,
+      useValue: {
+        disableImageSizeWarning: true
+      }
+    },
+  ]
 };
