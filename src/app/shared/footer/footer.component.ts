73a8351483cbeb9cdc068b364882280f701afdd4
Samuel Gagnon <118214762+SamLol12@users.noreply.github.com>
2025-09-19 00:07:22 -0400

    feat: enhance footer layout and styling, fix history section titles in English and French

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,16 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
-// CommonModule for DatePipe
-// RouterLink for routerLink attribute
 
 @Component({
     selector: 'app-footer',
+    standalone: true,
     imports: [CommonModule, RouterLink],
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
   myDate: Date = new Date();
-
 }
